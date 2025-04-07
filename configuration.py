54a7c48d21a89739197e5188b11facf1022d7f2c
Lilit-Simonyan <lilsimonyan01@gmail.com>
2025-04-07 13:19:26 +0400

    Corrected project

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = " https://a4649091-c661-41f5-8f6d-118b83231386.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://ab5fbff3-b19e-46bd-91b0-9f79ee8379a4.serverhub.praktikum-services.ru"
 CREATE_ORDER_PATH = "/api/v1/orders" 
 FIND_ORDER_TRACK = "/api/v1/orders/track"
--- test_order.py
+++ test_order.py
@@ -5,17 +5,12 @@ import data
 # 1. Создание заказа
 def test_create_order_and_find_by_track():
     response_create = sender.post_new_order(sender.data.order_body)
-    print(response_create.status_code)
-    print(response_create.json())
     assert response_create.status_code == 201
 
  # 2. Сохраняем track_id
     track_id = response_create.json().get("track")
-    print(track_id)
     assert track_id is not None
 
 # 3. Получение заказа по треку
     response_get = sender.get_order_by_track(track_id)
-    print(response_get.status_code)
-    print(response_get.json())
     assert response_get.status_code == 200
